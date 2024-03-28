23e4ef5051b810f8940402ea1dcb6f7ee011e9a7
Abigael Kirwa <abigael.kirwa@strathmore.edu>
2024-03-28 12:58:34 +0200

    feat(quick start):adding styles

--- homework2-reactdocumentation/src/App.js
+++ homework2-reactdocumentation/src/App.js
@@ -3,12 +3,12 @@ import './App.css';
 //creating a component and nesting
 function UserData(){
   return(
-    <>
+    <div className='user_details_wrapper'>
       <h1>User Details</h1>
       <p>Name: John Doe <br/>Jane Doe</p>
-      <img src='https://thispersondoesnotexist.com/'/>
+      <img className='user_image' src='https://thispersondoesnotexist.com/'/>
       <button >Click Me!</button>
-    </>
+    </div>
   )
 }
 
