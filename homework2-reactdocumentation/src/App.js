2b3696e8edfed794fd1a45546299e97cb694dcdb
Abigael Kirwa <abigael.kirwa@strathmore.edu>
2024-03-28 13:08:15 +0200

    feat(quick start):displaying data

--- homework2-reactdocumentation/src/App.js
+++ homework2-reactdocumentation/src/App.js
@@ -1,12 +1,19 @@
 import './App.css';
 
+//creating an object
+const user = {
+  male_name:"John Doe",
+  female_name: "Jane Doe",
+  img_source:"https://thispersondoesnotexist.com/"
+}
+
 //creating a component and nesting
 function UserData(){
   return(
     <div className='user_details_wrapper'>
       <h1>User Details</h1>
-      <p>Name: John Doe <br/>Jane Doe</p>
-      <img className='user_image' src='https://thispersondoesnotexist.com/'/>
+      <p>Name: {user.male_name} <br/> {user.female_name}</p>
+      <img className='user_image' src={user.img_source}/>
       <button >Click Me!</button>
     </div>
   )
